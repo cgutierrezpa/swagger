8d916232cf77fe85d52d32e4900b032af138736a
Rubén López Lozoya <rubenlopezlozoya12@gmail.com>
2017-07-09 17:58:25 +0200

    Cambio filename swagger.yaml por swagger.json para el proceso de iniciación de Swagger en server.js

--- server.js
+++ server.js
@@ -10,10 +10,11 @@ port = config.server.port || 3000;
 module.exports = app; // Required to perform tests over the API
 
 config.appRoot = __dirname; // Required by the Swagger specification.
+config.swaggerFile = 'api/swagger/swagger.json'; //By default, Swagger will look for swagger.yaml so we need to tell it to look for swagger.json
 
 SwaggerExpress.create(config, function(err, swaggerExpress) {
 	if (err) { throw err; }
-	
+
 	swaggerExpress.register(app);
 
   	db.connect(function(err) {
