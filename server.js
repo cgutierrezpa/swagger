a2ea05f60ae3a85a820b85d2314ae1a541e4738e
Rubén López Lozoya <rubenlopezlozoya12@gmail.com>
2017-07-09 11:51:55 +0200

    Reestructuración de ficheros Swagger

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 //'use strict';
-
 var SwaggerExpress = require('swagger-express-mw'),
 	env = process.env.NODE_ENV || 'test',
 	config = require('./config/config.js')[env],
@@ -32,8 +31,4 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
 			})
 		}
 	});
-  /*
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  }*/
 });
