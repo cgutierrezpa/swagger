ca60393c2f2c6f153091768b7a4e01a1f088a17b
Guti <100291121@alumnos.uc3m.es>
2017-07-14 21:30:35 +0200

    Integrada funcionalidad de autenticacion personalizada por JWT en swagger. Actualizadas las referencias de seguridad en la API

--- server.js
+++ server.js
@@ -6,19 +6,25 @@ express = require('express'),
 app = express(),
 cors = require('cors'),
 db = require('./lib/db.js'),
+authManager = require('./lib/authManager.js'),
 port = config.server.port || 3000;
 
 app.use(cors()); //Required to enable CORS in all requests (used by Swagger UI)
 
 module.exports = app; // Required to perform tests over the API
 
+
 config.appRoot = __dirname; // Required by the Swagger specification.
 config.swaggerFile = 'api/swagger/swagger.json'; //By default, Swagger will look for swagger.yaml so we need to tell it to look for swagger.json
+config.swaggerSecurityHandlers = {
+	Authorization: authManager.checkToken
+}
 
 SwaggerExpress.create(config, function(err, swaggerExpress) {
 	if (err) { throw err; }
 
 	swaggerExpress.register(app);
+
 
   	db.connect(function(err) {
 		if (err) {
