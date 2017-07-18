fa0d2d1c425b235d0bae47fdfcd39743fe5cedbb
Guti <100291121@alumnos.uc3m.es>
2017-07-18 19:39:24 +0200

    Traducida funcion de 'findUserById' para usar swagger

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -10,47 +10,51 @@ module.exports = {
 
 	/* Public methods */
 
-	findAll : function(req, res){
+	findAllUsers : function(req, res){
 		db.get().queryAsync('SELECT * FROM ' + db.tables.user + ' WHERE is_active = 1').then(function(rows){
 			if (rows.length == 0){
-				return res.status(404).json({"message": "No users found.\n"});
+				return res.status(404).json({"message": "No users found."});
 			}
 
 			return res.status(200).json(rows);
 		}).catch(function(err){
-			res.status(500).json({"message": "Internal server error.\n"});
+			res.status(500).json({"message": "Internal server error."});
 			throw err;
 		})
 	},
 
-	findById : function(req, res){
-		db.get().queryAsync('SELECT * FROM ' + db.tables.user + ' WHERE _id = ? AND is_active = 1', req.swagger.params.userId.value).then(function(rows){
+	findUserById : function(req, res){
+		db.get().queryAsync('SELECT * FROM ' + db.tables.user + ' WHERE _id = ? AND is_active = 1',req.swagger.params.userId.value)
+		.then(function(rows){
 			if (rows.length == 0){
-				return res.status(404).json("User not found.\n");
+				return res.status(404).json({"message": "User not found."});
 			}
+			delete rows[0]._id;
+			delete rows[0].tx_password;
+			delete rows[0].is_active;
+			delete rows[0].dt_create_timestamp;
 
-			return res.status(200).json(rows);
+			return res.status(200).json(rows[0]);
 		}).catch(function(err){
-			res.status(500).json("Internal server error.\n");
-			throw err;
+			errorhandler.internalServer(res, err);
 		})
 	},
 
-	create : async function(req, res) {
+	createUser : async function(req, res) {
 		try{
-			let fetchedUser = await module.exports.findByEmail(req.body.tx_email);
+			let fetchedUser = await module.exports.findByEmail(req.swagger.params.body.value.tx_email);
 
 			if (fetchedUser.length != 0){
-				return res.status(409).send('Email already exists.\n');
+				return res.status(409).send('Email already exists.');
 			}
 
 			var connection;
 			
-			bcrypt.hash(req.body.tx_password, 10)
+			bcrypt.hash(req.swagger.params.body.value.tx_password, 10)
 			.then(function(hash) {
 				let verificationData;
 				let response;
-				req.body.tx_password = hash;
+				req.swagger.params.body.value.tx_password = hash;
 				return db.get().getConnectionAsync();
 			})
 			.then(function(dbConnection){
@@ -58,14 +62,14 @@ module.exports = {
 				return connection.beginTransactionAsync();
 			})
 			.then(function(){
-				return connection.queryAsync('INSERT INTO ' + db.tables.user + ' SET ? ', req.body);
+				return connection.queryAsync('INSERT INTO ' + db.tables.user + ' SET ? ', req.swagger.params.body.value);
 			})
 			.then(function(rows){
 				return crypto.randomBytesAsync(128);
 			})
 			.then(function(buffer){
 				verificationData = {
-					tx_email: req.body.tx_email,
+					tx_email: req.swagger.params.body.value.tx_email,
 					tx_token: buffer.toString('hex'),
 					dt_expires_on: moment().add(1, 'days').format()
 				}
@@ -77,18 +81,18 @@ module.exports = {
 				return connection.commit();
 			})
 			.then(function(){
-				module.exports.sendVerificationEmail(req.body.tx_email);
+				module.exports.sendVerificationEmail(req.swagger.params.body.value.tx_email);
 				connection.release();
 				return res.status(200).send(response);
 			})
 			.catch(function(err){
 				if (connection != undefined){
 					connection.rollback(function(){
-						res.status(500).send("Internal server error.\n");
+						res.status(500).send({"message": "Internal server error."});
 						throw err;
 					});
 				}else{
-					res.status(500).send("Internal server error.\n");
+					res.status(500).send({"message": "Internal server error."});
 					throw err;
 				}
 			})
@@ -97,33 +101,33 @@ module.exports = {
 		}
 	},
 
-	update: async function(req, res){
+	updateUser: function(req, res){
 		//Falta añadir validación para que el usuario no pueda cambiar su email
-		db.get().queryAsync('UPDATE ' + db.tables.user + ' SET ? WHERE _id = ? ', [req.body, req.authInfo._id]).then(function(rows) {
+		db.get().queryAsync('UPDATE ' + db.tables.user + ' SET ? WHERE _id = ? ', [req.swagger.params.body.value, req.authInfo._id]).then(function(rows) {
 			if (rows.length == 0){
-				return res.status(404).send("User not found.\n");
+				return res.status(404).send({"message": "User not found."});
 			}
 
 			return res.status(200).send(rows);
 		})
 		.catch(function(err){
-			res.status(500).send("Internal server error.\n");
+			res.status(500).send({"message": "Internal server error."});
 			throw err;
 		})
 	},
 
-	delete : function(req, res) {
+	deleteUser : function(req, res) {
 		db.get().queryAsync('UPDATE ' + db.tables.user + ' SET is_active = 0 WHERE _id = ? ', req.authInfo._id).then(function(rows) {
 			return res.status(200).send();
 		})
 		.catch(function(err){
-			res.status(500).send("Internal server error.\n");
+			res.status(500).send({"message": "Internal server error."});
 		});
 	},
 
 	resetPassword: function(req, res){
 		let passwordResetData = {
-			tx_email: req.body.tx_email,
+			tx_email: req.swagger.params.body.value.tx_email,
 			tx_token: buffer.toString('hex'),
 			dt_expires_on: moment().add(1, 'days').format()
 		}
@@ -144,7 +148,7 @@ module.exports = {
 
 			let mailOptions = {
 				from: 'rubenlopezlozoya12@gmail.com',
-				to: req.body.tx_email,
+				to: req.swagger.params.body.value.tx_email,
 				subject: 'Flocker - Password reset',
 				html: '<h1>Password restoration</h1><p>Please, click in the link below to reset your password.</p><a href="http://localhost:3000/api/auth/password/reset' + passwordResetData.tx_token + '">Reset</a>'
 			};
@@ -159,14 +163,14 @@ module.exports = {
 		})
 		.catch(function(err){
 			if(err){
-				res.status(500).send("Internal server error.\n");
+				res.status(500).send({"message": "Internal server error."});
 				throw err;
 			}
 		});
 	},
 
 	changePassword : function(req, res){
-		bcrypt.hash(req.body.tx_password, 10)
+		bcrypt.hash(req.swagger.params.body.value.tx_password, 10)
 		.then(function(hash) {
 			return db.get().queryAsync('UPDATE ' + db.tables.user + ' SET tx_password = ? WHERE _id = ? ', [hash, req.authInfo._id])
 
@@ -176,7 +180,7 @@ module.exports = {
 		})
 		.catch(function(err){
 			if(err){
-				res.status(500).send("Internal server error.\n");
+				res.status(500).send({"message": "Internal server error."});
 				throw err;
 			}
 		});
